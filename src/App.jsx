32325e0c56a80df518ce474ae9b078eb213e8ee8
KralMarko123 <kralmarko123@gmail.com>
2023-12-12 00:38:46 +0100

    added home page and missing dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ROUTES } from './constants/ROUTES';
+import Home from './pages/Home';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
+			<Routes>
+				<Route path={ROUTES.HOME} element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
-export default App
+export default App;
